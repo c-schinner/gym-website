563531ba0c067ef7ff55fcfd63536bff4d52ec48
Chris <cschinner7@gmail.com>
2024-09-24 14:30:08 -0700

    added schedule component
    added the schedule component to the app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Carousel from "./components/Carousel"
 import Header from "./components/Header"
 import Programs from "./components/Programs"
+import Schedule from "./components/Schedule"
 import Trainers from "./components/Trainers"
 
 
@@ -18,8 +19,9 @@ function App() {
         <h1 className="md:text-5xl text-3xl font-semibold text-center p-10 text-gray-500">Fitness Classes:</h1>
         <Programs />
       </div>
-      <div>
-
+      <div id="schedule">
+        <h1 className="md:text-5xl text-3xl font-semibold text-center p-10 text-gray-500">Schedules:</h1>
+        <Schedule />
       </div>
     </div>
   )
